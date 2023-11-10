462638e11839dbb0f92635cb964bbaa8ddfe03dc
Jonathan Yang <hi@jyang.cc>
2023-11-10 15:32:30 +0100

    [chore] renaming when download

--- download_labels_from_lbv2_project.py
+++ download_labels_from_lbv2_project.py
@@ -5,10 +5,11 @@ from pathlib import Path
 import labelbox as lb
 import yaml
 
-from utility import save_lb_labels_json, train_val_test_split
+from utility import save_lb_labels_json
 
 # arguments
 override = True
+dataset_name = 'dtc2023'
 
 # read labelbox config
 lb_cfg = yaml.load(Path("labelbox_config.yaml").read_text(), Loader=yaml.FullLoader)
@@ -19,8 +20,9 @@ api_key = lb_cfg["LABELBOX_API_KEY"] or os.getenv("LABELBOX_API_KEY")
 save_dir = "labelbox_labels"
 client = lb.Client(api_key)
 project = client.get_project(PROJECT_ID)
-dest_dir = Path(save_dir) / project.name
+dest_dir = Path(save_dir) / dataset_name or project.name
 dest_dir.mkdir(parents=True, exist_ok=override)
+json_file_path = dest_dir / f"{dest_dir.stem}.json"
 
 # export labelbox labels
 project_export_task = project.export_v2()
@@ -34,7 +36,5 @@ if project_export_task.errors:
 else:
     print(f"Project export completed in {time.time() - start_time:.2f} seconds")
     labels = project_export_task.result
-    save_lb_labels_json(dest_dir / "labels.json", labels)
-    print(f"Saved labels to {dest_dir / 'labels.json'}")
-    # split and save labels
-    train_val_test_split(labels, project.name, dest_dir, override)
+    save_lb_labels_json(json_file_path, labels)
+    print(f"Saved labels to {json_file_path}")
